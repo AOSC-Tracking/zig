ab122113c0f89f9be7c88c5d809e3a09f4adbcd9
Alex Rønne Petersen <alex@alexrp.com>
2025-10-14 12:04:57 +0200

    libcxx: backport llvm/llvm-project#155476
    https: //github.com/llvm/llvm-project/pull/155476
    (cherry picked from commit 820dc9d76726ab9148b49630a1b4371624032918)
    Change-Id: I01c204c23df89dec6f7be874a079ddc54e376b36

--- lib/libcxx/include/__bit/bit_log2.h
+++ lib/libcxx/include/__bit/bit_log2.h
@@ -9,6 +9,7 @@
 #ifndef _LIBCPP___BIT_BIT_LOG2_H
 #define _LIBCPP___BIT_BIT_LOG2_H
 
+#include <__assert>
 #include <__bit/countl.h>
 #include <__config>
 #include <__type_traits/is_unsigned_integer.h>
@@ -25,6 +26,7 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 template <class _Tp>
 _LIBCPP_HIDE_FROM_ABI constexpr _Tp __bit_log2(_Tp __t) noexcept {
   static_assert(__libcpp_is_unsigned_integer<_Tp>::value, "__bit_log2 requires an unsigned integer type");
+  _LIBCPP_ASSERT_INTERNAL(__t != 0, "logarithm of 0 is undefined");
   return numeric_limits<_Tp>::digits - 1 - std::__countl_zero(__t);
 }
 
