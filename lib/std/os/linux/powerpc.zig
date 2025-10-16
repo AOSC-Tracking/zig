29443c040c504a1c40211d85650f149c563d9c65
Alex Rønne Petersen <alex@alexrp.com>
2025-10-16 23:15:23 +0200

    std.os.linux: remove some pointless asm clobbers in naked fns
    (cherry picked from commit d84faceebdce75648cc56c06c94ab404bc52e491)
    Change-Id: I3865f217ab557fbe000d8981ac2796d46b0fb6ea

--- lib/std/os/linux/powerpc.zig
+++ lib/std/os/linux/powerpc.zig
@@ -254,7 +254,7 @@ pub fn restore_rt() callconv(.naked) noreturn {
             :
             : [number] "i" (@intFromEnum(SYS.rt_sigreturn)),
             : .{ .memory = true, .cr0 = true, .r4 = true, .r5 = true, .r6 = true, .r7 = true, .r8 = true, .r9 = true, .r10 = true, .r11 = true, .r12 = true }),
-        else => _ = asm volatile (
+        else => asm volatile (
             \\ sc
             :
             : [number] "{r0}" (@intFromEnum(SYS.rt_sigreturn)),
