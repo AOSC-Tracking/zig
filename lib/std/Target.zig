86f6fec79a2a868a4d697d8b7325ec462f2aa2e5
Alex Rønne Petersen <alex@alexrp.com>
2025-10-19 22:31:08 +0200

    std.Target: update lists of omitted LLVM tags
    (cherry picked from commit e8d060e5f5b7a51cec101c4f36edc48f28510d14)

--- lib/std/Target.zig
+++ lib/std/Target.zig
@@ -68,6 +68,7 @@ pub const Os = struct {
 
         // LLVM tags deliberately omitted:
         // - bridgeos
+        // - cheriotrtos
         // - darwin
         // - kfreebsd
         // - nacl
@@ -768,14 +769,22 @@ pub const Abi = enum {
     // - coreclr
     // - domain
     // - geometry
+    // - gnueabit64
+    // - gnueabihft64
     // - gnuf64
+    // - gnut64
     // - hull
     // - intersection
     // - library
+    // - llvm
     // - mesh
     // - miss
+    // - mlibc
+    // - mtia
+    // - pauthtest
     // - pixel
     // - raygeneration
+    // - rootsignature
     // - vertex
 
     pub fn default(arch: Cpu.Arch, os_tag: Os.Tag) Abi {
@@ -1314,8 +1323,6 @@ pub const Cpu = struct {
         // - amdil
         // - amdil64
         // - dxil
-        // - le32
-        // - le64
         // - r600
         // - hsail
         // - hsail64
