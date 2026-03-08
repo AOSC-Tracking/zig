a7986903a9158b8a0e5f7c5d5e979bc3929bde1e
xtex <xtex@astrafall.org>
2026-03-08 10:30:02 +0800

    AOSC OS: add custom dev envs
    Change-Id: I3c22930523ec931e41d530e7ad62dd0ffd1bbf54

--- src/main.zig
+++ src/main.zig
@@ -5200,6 +5200,8 @@ fn cmdBuild(gpa: Allocator, arena: Allocator, args: []const []const u8) !void {
                 .have_zcu = true,
                 .emit_bin = true,
                 .is_test = false,
+
+                .use_llvm = if (dev.env == .@"aosc-bootstrap") true else null,
             });
 
             const root_mod = try Package.Module.create(arena, .{
